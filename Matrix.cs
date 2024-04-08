3859e76f8a7f7865da33d7ad23411e7d28cedeac
Marcel Owczarek <lerge.pl@gmail.com>
2024-04-08 03:40:35 -0700

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class Matrix
+    {
+        volatile int[,] matrix;
+        int size { get; set; }
+
+        public Matrix(int size, int seed)
+        {
+            this.size = size;
+            this.matrix = new int[size, size];
+            Random random = new Random(seed);
+            int value;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    value = random.Next(1, 10);
+                    this.matrix[i, j] = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            string display = "";
+            for (int i = 0; i < this.size; i++)
+            {
+                for (int j = 0; j < this.size; j++)
+                {
+                    display += matrix[i, j].ToString() + " ";
+                }
+                display += "\n";
+            }
+            return display;
+        }
+        public void RowTimesCol(Matrix matrixA, Matrix matrixB, int row, int col)
+        {
+            int sum = 0;
+            if (matrixA.size == matrixB.size)
+            {
+                int size = this.size;
+                for (int i = 0; i < size; i++)
+                {
+                    sum += matrixA.matrix[row, i] * matrixB.matrix[i, col];
+                }
+            }
+            this.matrix[row, col] = sum;
+            sum = 0;
+        }
+        public void Multiply(Matrix matrixA, Matrix matrixB, int numOfThreads)
+        {
+            Thread[] threads = new Thread[numOfThreads];
+            int step = this.size / numOfThreads;
+            int rest = this.size % numOfThreads;
+            int begin = 0, end = 0;
+
+            for ( int n = 0; n < numOfThreads; n++)
+            {
+                begin = end;
+                if (n == numOfThreads - 1) { end += step + rest; }
+                else { end += step; }
+                Console.WriteLine("nn:" + n + " bb:" + begin + " ee:" + end);
+                //Console.WriteLine(n);
+                //Console.WriteLine(begin +" "+ end);
+                threads[n] = new Thread(() =>
+                {
+                    int threadBegin = begin;
+                    int threadEnd = end;
+                    int num = n;
+                    for (int i = threadBegin; i < threadEnd; i++)
+                    {
+                        for (int j = 0; j < this.size; j++)
+                        {
+                            this.RowTimesCol(matrixA, matrixB, i, j);
+                        }
+                    }
+                    Console.WriteLine("n:" + num + " b:" + threadBegin + " e:" + threadEnd);
+                    Console.WriteLine(ToString());
+                });
+                threads[n].Name = String.Format("Thread: {0}", n);
+                //Console.WriteLine(n);
+                //Console.WriteLine(begin + " " + end);
+                threads[n].Start();
+                //threads[n].Join();
+            }
+            //foreach (Thread x in threads) { Console.WriteLine("Start"); x.Start(); }
+            foreach (Thread thread in threads) { thread.Join(); Console.WriteLine("stop"); }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Podaj wielkosc macierzy:");
+            //int size = int.Parse(Console.ReadLine());
+
+            Matrix matrixA = new Matrix(3,7);
+            Matrix matrixB = new Matrix(3,9);
+            Console.WriteLine(matrixA);
+            Console.WriteLine(matrixB);
+
+            //for (int i = 2; i < 10; i++) {
+            //    Matrix matrixC = new Matrix(2, 0);
+            //    DateTime startTime = DateTime.Now;
+
+            //    matrixC.Multiply(matrixA, matrixB, i);
+
+            //    DateTime stopTime = DateTime.Now;
+            //    TimeSpan roznica = stopTime - startTime;
+            //    Console.WriteLine("Czas pracy dla n= :" + i + " watkow:" + roznica.TotalMilliseconds);
+            //}
+            Matrix matrixC = new Matrix(3,0);
+            matrixC.Multiply(matrixA, matrixB, 3);
+            Console.WriteLine(matrixC);
+        }
+    }
+}
