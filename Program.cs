4e9b82a93335114b2093a640ddd5baa08f89a086
Marcel Owczarek <lerge.pl@gmail.com>
2024-04-11 18:11:03 -0700

    final bash with time tests

--- Program.cs
+++ Program.cs
@@ -7,33 +7,53 @@ namespace Threads
     {
         static void Main(string[] args)
         {
-            int size = 1000;
-            int numOfThreads = 2;
+            int size = 500;
+            int numOfMaxThreads = 10;
             int repeat = 10;
-            Matrix matrixA = new Matrix(size,7);
-            Matrix matrixB = new Matrix(size,9);
-            Matrix matrixC = new Matrix(size);
-            Matrix matrixD = new Matrix(size);
 
-            //Console.WriteLine(matrixA);
-            //Console.WriteLine(matrixB);
+            for (int numOfThreads = 1; numOfThreads < numOfMaxThreads; numOfThreads++) {
 
-            DateTime startThreadTime = DateTime.Now;
-                matrixC = ThreadMatrix.Multiply(matrixA, matrixB, numOfThreads);
-                DateTime stopThreadTime = DateTime.Now;
+                double sumOfThreadTime = 0;
+                double sumOfParallelTime = 0;
 
-                TimeSpan ThreadTime = stopThreadTime - startThreadTime;
-                Console.WriteLine("Czas pracy Threads dla n " + numOfThreads + " watkow:" + ThreadTime.TotalMilliseconds + " ms");
-                //Console.WriteLine(matrixC);
+                Console.WriteLine("Thread");
+                Console.WriteLine("size: \t numOfThreads: \t time:");
+                for (int i = 0; i < repeat; i++)
+                {
+                    Matrix matrixA = new Matrix(size, 7);
+                    Matrix matrixB = new Matrix(size, 9);
+                    Matrix matrixC = new Matrix(size);
 
-                DateTime startParallelTime = DateTime.Now;
-                matrixD = ParallelMatrix.Multiply(matrixA, matrixB, numOfThreads);
-                DateTime stopParallelTime = DateTime.Now;
+                    DateTime startThreadTime = DateTime.Now;
+                    matrixC = ThreadMatrix.Multiply(matrixA, matrixB, numOfThreads);
+                    DateTime stopThreadTime = DateTime.Now;
 
-                TimeSpan ParallelTime = stopParallelTime - startParallelTime;
-                Console.WriteLine("Czas pracy Parallel dla n " + numOfThreads + " watkow:" + ParallelTime.TotalMilliseconds + " ms");
-                //Console.WriteLine(matrixC);
-            
+                    TimeSpan ThreadTime = stopThreadTime - startThreadTime;
+                    //Console.WriteLine("Czas pracy Threads dla n " + numOfThreads + " watkow: " + ThreadTime.TotalMilliseconds + " ms, wielkosc macierzy size = " + size);
+                    Console.WriteLine(size + "\t" + numOfThreads + "\t" + ThreadTime.TotalMilliseconds);
+                    sumOfThreadTime += ThreadTime.TotalMilliseconds;
+                }
+                Console.WriteLine("average = " + sumOfThreadTime / repeat + " ms");
+
+                Console.WriteLine("Parallel");
+                Console.WriteLine("size: \t numOfThreads: \t time:");
+                for (int i = 0; i < repeat; i++) {
+                    Matrix matrixA = new Matrix(size, 7);
+                    Matrix matrixB = new Matrix(size, 9);
+                    Matrix matrixD = new Matrix(size);
+
+                    DateTime startParallelTime = DateTime.Now;
+                    matrixD = ParallelMatrix.Multiply(matrixA, matrixB, numOfThreads);
+                    DateTime stopParallelTime = DateTime.Now;
+
+                    TimeSpan ParallelTime = stopParallelTime - startParallelTime;
+                    //Console.WriteLine("Czas pracy Parallel dla n " + numOfThreads + " watkow: " + ParallelTime.TotalMilliseconds + " ms, wielkosc macierzy size = " + size);
+                    Console.WriteLine(size + "\t" + numOfThreads + "\t" + ParallelTime.TotalMilliseconds);
+                    sumOfParallelTime += ParallelTime.TotalMilliseconds;
+                }
+                Console.WriteLine("average = " + sumOfParallelTime / repeat + " ms");
+
+            }
         }
     }
     }
